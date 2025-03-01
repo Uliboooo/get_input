d1ef6982c2bb457746cb030621ad8e9c6a55aa4b
coyuki <fu08yu20ko07@icloud.com>
2025-03-01 22:26:37 +0900

    add func that yes_no()

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 use std::io::{stdout, Write};
 
 /// get user's input. return String.
-/// 
+///
 /// ## how to get input without message.
 /// ```
 /// let input = get_input("");
 /// ```
-/// 
+///
 /// ## how to get input with message.
 /// ```
 /// let input = get_input("please title>");
 /// ```
-/// 
+///
 /// ```bash
 /// // 👇console
 /// please title>foo 👈foo is user's input.
@@ -24,7 +24,12 @@ pub fn get_input<S: AsRef<str>>(message: S) -> String {
     std::io::stdin()
         .read_line(&mut word)
         .expect("Failed to read line");
-        word.trim().to_string()
+    word.trim().to_string()
+}
+
+/// if user's input is "y" or "yes", return true.
+pub fn yes_no<S: AsRef<str>>(message: S) -> bool {
+    matches!(get_input(message.as_ref()).as_str(), "y" | "yes")
 }
 
 #[cfg(test)]
